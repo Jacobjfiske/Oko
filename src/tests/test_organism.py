61a2cad16be01c46ecc8c4b5009ffedf114568ab
jacobfiske <jacobjfiske@gmail.com>
2026-08-28 01:05:11 -0600

    test: added more time and death cases.

---
+++ src/tests/test_organism.py
@@ -0,0 +1,29 @@
+import unittest
+
+from src.ecosim.simulation.genome import Genome
+from src.ecosim.simulation.organism import Organism
+
+
+class TestOrganism(unittest.TestCase):
+
+    def test_organism_dies_when_energy_reaches_zero(self):
+        genome = Genome(
+            size=1,
+            speed=1,
+            vision=100,
+            metabolism=1,
+        )
+        organism = Organism(
+            x=100,
+            y=100,
+            genome=genome,
+            energy=1,
+        )
+
+        organism.update(dt=1)
+
+        self.assertFalse(organism.alive)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/tests/test_world.py
+++ src/tests/test_world.py
@@ -1,28 +1,57 @@
 import unittest
-
 from src.ecosim.simulation.genome import Genome
 from src.ecosim.simulation.organism import Organism
+from src.ecosim.simulation.world import World
 
 
-class TestOrganism(unittest.TestCase):
+class TestWorld(unittest.TestCase):
 
-    def test_organism_dies_when_energy_reaches_zero(self):
-        genome = Genome(
+    def setUp(self):
+        self.world = World()
+
+        self.genome = Genome(
             size=1,
             speed=1,
             vision=100,
             metabolism=1,
         )
+
+    def test_update_advances_time(self):
+        self.world.update(1)
+        self.assertEqual(self.world.time, 1)
+
+    def test_update_removes_organism_when_energy_reaches_zero(self):
         organism = Organism(
             x=100,
             y=100,
-            genome=genome,
-            energy=1,
+            genome=self.genome,
+            alive=True,
+            energy=1
         )
 
-        organism.update(dt=1)
+        self.world.organisms = [organism]
+        self.world.update(dt=1)
+        self.assertEqual(len(self.world.organisms), 0)
 
-        self.assertTrue(organism.alive)
+    def test_remove_dead(self):
+        alive_organism = Organism(
+            x=100,
+            y=100,
+            genome=self.genome,
+            alive=True,
+        )
+        dead_organism = Organism(
+            x=200,
+            y=200,
+            genome=self.genome,
+            alive=False,
+        )
+
+        self.world.organisms = [alive_organism, dead_organism]
+        self.world.remove_dead()
+
+        self.assertEqual(len(self.world.organisms), 1)
+        self.assertIs(self.world.organisms[0], alive_organism)
 
 
 if __name__ == "__main__":
