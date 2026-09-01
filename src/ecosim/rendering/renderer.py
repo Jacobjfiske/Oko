6226be99cd2c8e56bdb03b0d90dd45870252ce66
jacobfiske <jacobjfiske@gmail.com>
2026-09-01 16:54:13 -0600

    feat: render corpses

--- src/ecosim/rendering/renderer.py
+++ src/ecosim/rendering/renderer.py
@@ -17,6 +17,13 @@ class Renderer:
                 2
             )
 
+        for corpse in world.corpses:
+            pygame.draw.polygon(
+                self.screen,
+                (255, 0, 0),
+                self._calculate_diamond_points(corpse.x, corpse.y,4),
+                0)
+
         for organism in world.organisms:
             pygame.draw.circle(
                 self.screen,
@@ -24,3 +31,12 @@ class Renderer:
                 (int(organism.x), int(organism.y)),
                 int(organism.genome.size * 4),
             )
+
+    @staticmethod
+    def _calculate_diamond_points(center_x, center_y, size):
+        top_point = center_x, center_y - size
+        bottom_point = center_x, center_y + size
+        left_point = center_x - size, center_y
+        right_point = center_x + size, center_y
+
+        return [top_point, right_point, bottom_point, left_point]
